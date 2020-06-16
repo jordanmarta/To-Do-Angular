68d11480714b8a93d11deecc1d88f7304475f3cc
jordan <jordan@gmail.com>
2020-06-15 22:09:57 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+// metadados para a classe
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root', // <app-root>
+	templateUrl: './app.component.html', // define o html do componente
+	// template: '<p>meu template</p>', // posso definir o html aqui dentro caso seja preciso
+	styleUrls: [ './app.component.css' ] // define o css do componente
 })
 export class AppComponent {
-  title = 'todo';
+	public todos: any[] = [];
+	public title: String = 'Minhas tarefas';
+
+	/**
+   * ctor
+   */
+	constructor() {
+		this.todos.push('passear com o cachorro');
+		this.todos.push('ir ao supermercado');
+		this.todos.push('cortar o cabelo');
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+//decorator (gera informações adicionais p classe)
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [ AppComponent ], // componentes q terei no módulo
+	imports: [ BrowserModule ], // coisas extras q irei utilizar
+	providers: [], // tudo q vou prover para os componentes, como serviços, etc..
+	bootstrap: [ AppComponent ] //primeiro componente a ser iniciado
 })
-export class AppModule { }
+export class AppModule {}
